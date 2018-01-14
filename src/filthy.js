6173dafad0266ee7ad69d3e9cc5f2ce13fa3efa5
Claudio Semeraro <raptxt@gmail.com>
2018-01-14 20:03:52 +0100

    chore: linting

--- src/filthy.js
+++ src/filthy.js
@@ -1,15 +1,15 @@
-const ALLOWED_TAGS = 'div,p,a,br,i,em,strong,b,img';
-const ALLOWED_ATTRS = 'href,title,alt,src,width,height';
+const ALLOWED_TAGS = "div,p,a,br,i,em,strong,b,img";
+const ALLOWED_ATTRS = "href,title,alt,src,width,height";
 
 const createDoc = () => document.implementation.createHTMLDocument();
-const createDiv = (doc) => doc.createElement('div');
+const createDiv = doc => doc.createElement("div");
 
 let doc;
 
 function cleanAttribute(name, content) {
-	if(['href', 'src'].indexOf(name) < 0) return content;
+	if (["href", "src"].indexOf(name) < 0) return content;
 	const urlPattern = /^((http|https|ftp):\/\/)/;
-	if(!urlPattern.test(content)) return '';
+	if (!urlPattern.test(content)) return "";
 	return content;
 }
 
@@ -17,10 +17,13 @@ function cleanNode(originalNode, allowedAttrs) {
 	const nodeName = originalNode.nodeName.toLowerCase();
 	const nodeCopy = doc.createElement(nodeName);
 
-	for(let n = 0; n < originalNode.attributes.length; n++) {
+	for (let n = 0; n < originalNode.attributes.length; n++) {
 		const attrName = originalNode.attributes.item(n).name;
-		if(allowedAttrs.indexOf(attrName) > -1) {
-			const attr = cleanAttribute(attrName, originalNode.getAttribute(attrName))
+		if (allowedAttrs.indexOf(attrName) > -1) {
+			const attr = cleanAttribute(
+				attrName,
+				originalNode.getAttribute(attrName)
+			);
 			nodeCopy.setAttribute(attrName, attr);
 		}
 	}
@@ -30,19 +33,19 @@ function cleanNode(originalNode, allowedAttrs) {
 
 function filter(node, opts = {}) {
 	const nodeName = node.nodeName.toLowerCase();
-	const allowedNodes = opts.allowedNodes || ALLOWED_TAGS.split(',');
-	const allowedAttrs = opts.allowedAttrs || ALLOWED_ATTRS.split(',');
+	const allowedNodes = opts.allowedNodes || ALLOWED_TAGS.split(",");
+	const allowedAttrs = opts.allowedAttrs || ALLOWED_ATTRS.split(",");
 
-	if(nodeName === '#text') return node;
-	if(nodeName === '#comment') return doc.createTextNode('');
-	if(allowedNodes.indexOf(nodeName) < 0) return doc.createTextNode('');
+	if (nodeName === "#text") return node;
+	if (nodeName === "#comment") return doc.createTextNode("");
+	if (allowedNodes.indexOf(nodeName) < 0) return doc.createTextNode("");
 
 	const nodeCopy = cleanNode(node, allowedAttrs);
 
 	while (node.childNodes.length > 0) {
-    const child = node.removeChild(node.childNodes[0]);
-    nodeCopy.appendChild(filter(child, opts));
-  }
+		const child = node.removeChild(node.childNodes[0]);
+		nodeCopy.appendChild(filter(child, opts));
+	}
 
 	return nodeCopy;
 }
--- src/filthy.test.js
+++ src/filthy.test.js
@@ -1,6 +1,6 @@
-const sanitizeHtml = require('./filthy');
-const jsdom = require('jsdom').JSDOM;
-const { document } = (new jsdom('<!DOCTYPE html><p>Hello world</p>')).window;
+const sanitizeHtml = require("./filthy");
+const jsdom = require("jsdom").JSDOM;
+const { document } = new jsdom("<!DOCTYPE html><p>Hello world</p>").window;
 
 const filthyString = `
 	<div class="xss" onload="javascript:doEvil();" aria-label="eval('alert('\'boo\)')">
@@ -17,30 +17,34 @@ const filthyString = `
 	</div>
 `;
 
-describe('filthy', () => {
-	it('sanitizes an HTML string to be safely embedded', () => {
-		const expected = `<div> <p> Lorem <strong>Ipsum</strong> dolor <b>sit</b> <em>amet</em><br> and <i>italic</i> Go to <a href=\"http://github.com/keepitterron\" title=\"Keepitterron's github\">this profile</a> </p> <img src=\"http://keepitterron.github.io/keepitterron/avatar.png\" alt=\"An image\" width=\"700\" height=\"600\"> <a href=\"\">this won't run</a> </div>`;
-		const res = sanitizeHtml(filthyString);
+describe("filthy", () => {
+  it("sanitizes an HTML string to be safely embedded", () => {
+    const expected = `<div> <p> Lorem <strong>Ipsum</strong> dolor <b>sit</b> <em>amet</em><br> and <i>italic</i> Go to <a href=\"http://github.com/keepitterron\" title=\"Keepitterron's github\">this profile</a> </p> <img src=\"http://keepitterron.github.io/keepitterron/avatar.png\" alt=\"An image\" width=\"700\" height=\"600\"> <a href=\"\">this won't run</a> </div>`;
+    const res = sanitizeHtml(filthyString);
 
-		expect(formatHtmlString(res)).toBe(expected);
-	});
+    expect(formatHtmlString(res)).toBe(expected);
+  });
 
-	it('uses custom allowNodes if provided', () => {
-		const expected = '<div> <link href=\"http://github.com/style.css\"> </div>';
-		const res = sanitizeHtml(filthyString, {allowedNodes: ['div', 'link']});
+  it("uses custom allowNodes if provided", () => {
+    const expected = '<div> <link href="http://github.com/style.css"> </div>';
+    const res = sanitizeHtml(filthyString, { allowedNodes: ["div", "link"] });
 
-		expect(formatHtmlString(res)).toBe(expected);
-	});
+    expect(formatHtmlString(res)).toBe(expected);
+  });
 
-	it('uses custom allowAttrs if provided', () => {
-		const expected = '<div> <img alt=\"An image\"> </div>';
-		const res = sanitizeHtml(filthyString, {allowedNodes: ['div', 'img'], allowedAttrs: ['alt']});
+  it("uses custom allowAttrs if provided", () => {
+    const expected = '<div> <img alt="An image"> </div>';
+    const res = sanitizeHtml(filthyString, {
+      allowedNodes: ["div", "img"],
+      allowedAttrs: ["alt"]
+    });
 
-		expect(formatHtmlString(res)).toBe(expected);
-	});
+    expect(formatHtmlString(res)).toBe(expected);
+  });
 });
 
-const formatHtmlString = str => str
-	.trim()
-	.replace(/\n|\r/g, '')
-	.replace(/\s\s+/g, ' ');
+const formatHtmlString = str =>
+  str
+    .trim()
+    .replace(/\n|\r/g, "")
+    .replace(/\s\s+/g, " ");
